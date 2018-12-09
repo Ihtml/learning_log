fb2dd8e9515cf70cfb86262c749559faf44aea25
Ihtml <2483690020@qq.com>
2018-12-10 00:06:50 +0800

    add base.html topics.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 
 
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    all_topics = Topic.objects.order_by('date_added')
+    context = {'topics': all_topics}
+    return render(request, 'learning_logs/topics.html', context)
+
