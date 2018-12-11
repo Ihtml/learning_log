54129aaa65427691b2755dda9b140022f76d4fd1
Ihtml <2483690020@qq.com>
2018-12-12 00:44:34 +0800

    login page

--- users/urls.py
+++ users/urls.py
@@ -3,12 +3,11 @@ __author__ = 'ife'
 __date__ = '2018-12-11 23:52'
 
 from django.urls import path
-from django.contrib.auth.views import login
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 
-app_name = 'learning_logs'
+app_name = 'users'
 urlpatterns = [
-    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),  name='login'),
 ]
