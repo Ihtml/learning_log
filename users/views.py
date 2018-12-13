47fc3cd4e37e3e3f48dfe4bb030697472d29dbdc
Ihtml <2483690020@qq.com>
2018-12-13 21:59:25 +0800

    register user

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def logout_view(request):
@@ -12,3 +13,22 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
 
+
+def register(request):
+    # 注册新用户
+    if request.method != 'POST':
+        # 显示空的注册表单
+        form = UserCreationForm()
+    else:
+        # 处理填好的表单
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # 让用户自动登录，再重定向到主页,authenticate是用户证书，在密码对给出的用户名合法的情况下返回一个 User对象
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
